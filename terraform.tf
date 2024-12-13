e2d561619087241273674e519bd3bb35ee752a59
Gene.Li <gene.li@ecloudrover.com>
2024-12-13 18:24:59 +0800

    a

--- terraform.tf
+++ terraform.tf
@@ -11,10 +11,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = "~> 5.0.0"
     }
   }
 
-  required_version = "~> 1.2"
+  required_version = "~> 1.0"
 }
 
