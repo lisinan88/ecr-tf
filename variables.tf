6019f5556bf83adcaa9595783232138d5fbcff48
Gene.Li <gene.li@ecloudrover.com>
2024-12-13 10:34:19 +0800

    asd

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@
 
 variable "region" {
   description = "AWS region"
-  default     = "us-west-2"
+  default     = "us-west-1"
   }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "instance_name" {
