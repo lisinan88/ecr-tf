f4168bd512944f34bf18ece3b80204ff08dd1ba6
Gene.Li <gene.li@ecloudrover.com>
2024-12-16 17:03:56 +0800

    newchange

--- main.tf
+++ main.tf
@@ -39,8 +39,6 @@ module "web_server_sg" {
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = var.vpc_id
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 8080
@@ -54,4 +52,14 @@ module "web_server_sg" {
       cidr_blocks = "0.0.0.0/0"
     },
   ]
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "var.vpc_id"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
 }
