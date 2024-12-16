33f233c4b8e92a439e3cbd3ac919b8f1057ca5a5
Gene.Li <gene.li@ecloudrover.com>
2024-12-16 16:51:14 +0800

    change Security Group

--- main.tf
+++ main.tf
@@ -39,6 +39,19 @@ module "web_server_sg" {
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = var.vpc_id
-
-  ingress_cidr_blocks = ["10.10.0.0/16"]
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 }
