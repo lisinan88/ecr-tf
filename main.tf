fb511ec675150eb9a6f42b5caa51d46decc343f7
Gene.Li <gene.li@ecloudrover.com>
2025-01-16 23:38:56 +0800

    ads

--- main.tf
+++ main.tf
@@ -5,7 +5,19 @@ provider "aws" {
   region = var.region
 }
 
+# 这个是一个注释，会根据自己的AWS账号自动获取AMI,然后赋予变量
+    data "aws_ami" "ubuntu" {
+      most_recent = true
 
+      owners = ["self"]
+      tags = {
+        Name   = "nginx-test"
+        Tested = "true"
+      }
+    }
+# control tab 代码右移动一行
+# shift + tab 代码左移动一行
+# ctrl + / 单行注释
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -15,7 +27,6 @@ data "aws_ami" "ubuntu" {
     Tested = "true"
   }
 }
-
 
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
